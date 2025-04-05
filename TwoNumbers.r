f2d831bd76d91b51e59200fa8b15ef4860a8bd0e
Maika Ybiza <117846947+mikeyswifedjk@users.noreply.github.com>
2025-04-05 18:30:01 +0800

    first commit

---
+++ TwoNumbers.r
@@ -0,0 +1,8 @@
+
+num1 <- as.numeric(readline(prompt = "Enter the first number: "))
+num2 <- as.numeric(readline(prompt = "Enter the second number: "))
+
+# Calculate the sum
+sum <- num1 + num2
+
+cat("The sum of", num1, "and", num2, "is:", sum, "\n")
