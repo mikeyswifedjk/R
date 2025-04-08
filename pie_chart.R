7bbe187ddf7dfaf3b4a01b7b3f01740644431677
Maika Ybiza <117846947+mikeyswifedjk@users.noreply.github.com>
2025-04-08 10:03:41 +0800

    feature: pie chart

---
+++ pie_chart.R
@@ -0,0 +1,6 @@
+#Pie Chart of social media usage
+social_media_usage <- c(8, 5, 10, 7, 3, 8, 10, 12, 8, 10, 3, 10, 8, 8, 10, 6, 12, 12, 12, 7)
+
+name <- c("djenna", "harvey", "marilou", "jodilyn", "ronald", "david", "christel", "eva", "daniela", "joan", "ralph", "john mark", "ghem", "gellie", "renzo", "ella", "erick", "maybilline", "albert", "maika")
+
+pie(social_media_usage, labels = name, main = "Social Media Usage")
